4a7b0a1d35bf206d021153cff0d1821d5aba6df5
Sandip Rijal <sandurijal03@hotmail.com>
2024-01-23 18:52:40 +0545

    client: added dummy creating join buttons

--- client/src/pages/videoRooms/VideoRooms.tsx
+++ client/src/pages/videoRooms/VideoRooms.tsx
@@ -13,10 +13,76 @@ const CallImg = styled.img`
   height: 40px;
 `
 
+type RoomJoinButtonProps = {
+  creatorUsername: string
+  roomId: number | string
+  amountOfParticipants: number
+}
+
+const RoomJoinButton: React.FC<RoomJoinButtonProps> = ({
+  creatorUsername,
+  roomId,
+  amountOfParticipants,
+}) => {
+  const handleJoinRoom = () => {}
+  return <RoomJoinBtn>{creatorUsername[0]}</RoomJoinBtn>
+}
+
+const RoomJoinBtn = styled.button`
+  width: 45px;
+  height: 45px;
+  margin-right: 15px;
+  border-radius: 45px;
+  background-color: #00ffff;
+  font-size: 24px;
+  border: none;
+  transition: 0.3s;
+
+  :hover {
+    opacity: 0.6;
+  }
+
+  :active {
+    background-color: #000;
+  }
+`
+
+const DUMMY_ROOMS = [
+  {
+    id: 1,
+    participants: [
+      {
+        socketId: 1,
+        peerId: 1,
+        username: 'Martin',
+      },
+    ],
+  },
+  {
+    id: 2,
+    participants: [
+      {
+        socketId: 2,
+        peerId: 2,
+        username: 'John',
+      },
+    ],
+  },
+]
+
 const RoomsList = () => {
   const rooms = useSelector((state: any) => state.videoRoom.rooms)
   return (
     <div>
+      {DUMMY_ROOMS.map((room) => (
+        <RoomJoinButton
+          key={room.id}
+          creatorUsername={room.participants[0].username}
+          roomId={room.id}
+          amountOfParticipants={room.participants.length}
+        />
+      ))}
+
       <CreateRoomButton />
     </div>
   )

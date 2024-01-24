a222e5f7fdb92df7ce7e8aceeab4e2820f78beec
Sandip Rijal <sandurijal03@hotmail.com>
2024-01-24 16:32:40 +0545

    client: connection with peer and creating peerId

--- client/src/realtimeCommunication/webRTCHandler.ts
+++ client/src/realtimeCommunication/webRTCHandler.ts
@@ -1,3 +1,8 @@
+import { Peer } from 'peerjs'
+
+let peer
+let peerId
+
 import store from '../store/store'
 import { setLocalStream } from '../store/videoroom/videoroomSlice'
 
@@ -13,4 +18,17 @@ export const getAccessToLocalStream = async () => {
   }
 
   return Boolean(localStream)
+}
+
+export const connectWithPeerServer = () => {
+  peer = new Peer('', {
+    host: 'localhost',
+    port: 5000,
+    path: '/peer',
+  })
+
+  peer.on('open', (id) => {
+    console.log('peer id', id)
+    peerId = id;
+  })
 }
